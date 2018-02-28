5d14dc288225900da879e75db29d609d6728d88b
lhx1228 <1040115517@qq.com>
2018-02-28 10:56:01 +0800

    main文件

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
-import tkinter,os,pygame
+import tkinter
+import os
+import pygame
 from subprocess import run
 from tkinter import *
 from tkinter import filedialog
@@ -9,30 +11,33 @@ ly_flag = 0
 textLyric = ''
 e1 = ''
 
-def MP3towav():
-    run('ffmpeg -i "{}.mp3" "{}.wav"'.format(e1.get(),e1.get()),shell=True)
 
-    run('attrib "{}.wav" +s +h'.format(e1.get()),shell=True)
-        
+def MP3towav():
+    run('ffmpeg -i "{}.mp3" "{}.wav"'.format(e1.get(), e1.get()), shell=True)
+
+    run('attrib "{}.wav" +s +h'.format(e1.get()), shell=True)
+
     os.remove('{}.mp3'.format(e1.get()))
     playMusic()
     adjust()
 
+
 def run_songID():
     global ly_flag
     ly_flag = 1
-    #print(e1.get())
-    run('python C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/get_songID.py {}'.format(e1.get()),shell=True)
+    # print(e1.get())
+    run('python C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/get_songID.py {}'.format(e1.get()), shell=True)
     if os.path.exists('{}.wav'.format(e1.get())):
         playMusic()
     else:
         MP3towav()
-    
+
+
 def playMusic():
     pygame.mixer.init()
     pygame.mixer.music.load('{}.wav'.format(e1.get()))
     pygame.mixer.music.play()
-    
+
 
 def pause_music():
     global flag
@@ -43,21 +48,25 @@ def pause_music():
         pygame.mixer.music.unpause()
         flag = 0
 
+
 def Input_songName():
     global e1
-    frame= tkinter.Frame(master)
-    frame.pack(side=tkinter.LEFT,fill=tkinter.Y)
-    lv= tkinter.StringVar()
-    listBox= tkinter.Listbox(frame,selectmode=tkinter.BROWSE,width=30,height=30,bg="#FFFFFF",listvariable=lv)  
+    frame = tkinter.Frame(master)
+    frame.pack(side=tkinter.LEFT, fill=tkinter.Y)
+    lv = tkinter.StringVar()
+    listBox = tkinter.Listbox(frame, selectmode=tkinter.BROWSE,
+                              width=30, height=30, bg="#FFFFFF", listvariable=lv)
     listBox.pack()
-    e1 = Entry(frame,bg='#FFFFFF')
-    #e1.grid()
-    e1.place(x=50,y=150)
+    e1 = Entry(frame, bg='#FFFFFF')
+    # e1.grid()
+    e1.place(x=50, y=150)
     songName = e1.get()
-    tkinter.Label(master, text="搜索",fg='#000000',bg="#FFFFFF").place(x=10,y=148)
-    Button(master, text='播放',command=run_songID).place(x=50,y=180)
-    Button(master, text='暂停',command=pause_music).place(x=140,y=180)
-    #adjust()
+    tkinter.Label(master, text="搜索", fg='#000000',
+                  bg="#FFFFFF").place(x=10, y=148)
+    Button(master, text='播放', command=run_songID).place(x=50, y=180)
+    Button(master, text='暂停', command=pause_music).place(x=140, y=180)
+    # adjust()
+
 
 def adjust():
     if ly_flag == 0:
@@ -70,39 +79,40 @@ def adjust():
 
         '''
     elif ly_flag == 1:
-        with open('C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/lyrics.txt','r') as file:
+        with open('C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/lyrics.txt', 'r') as file:
             lyrics_text = file.read()
         lyrics = '''
         {}
         '''.format(lyrics_text)
-        textLyric.delete(0.0,END)#Text清空
-        
+        textLyric.delete(0.0, END)  # Text清空
+
     textLyric.insert(END, lyrics)
 
 
 def Init():
     global textLyric
-    frame= tkinter.Frame(master)
-    frame.pack(side=tkinter.TOP,fill=tkinter.Y)
+    frame = tkinter.Frame(master)
+    frame.pack(side=tkinter.TOP, fill=tkinter.Y)
     S = tkinter.Scrollbar(frame)
-    textLyric = tkinter.Text(frame,bg="#FFFFFF",height=50)
-    S.pack(side=RIGHT,fill=Y)
+    textLyric = tkinter.Text(frame, bg="#FFFFFF", height=50)
+    S.pack(side=RIGHT, fill=Y)
     textLyric.pack(side=LEFT, fill=Y)
     S.config(command=textLyric.yview)
     textLyric.config(yscrollcommand=S.set)
     adjust()
     menu()
 
+
 def print_lyrics():
-    frame= tkinter.Frame(master)
-    frame.pack(side=tkinter.TOP,fill=tkinter.Y)
+    frame = tkinter.Frame(master)
+    frame.pack(side=tkinter.TOP, fill=tkinter.Y)
     S = tkinter.Scrollbar(frame)
-    textLyric= tkinter.Text(frame,bg="#FFFFFF",height=50)
-    S.pack(side=RIGHT,fill=Y)
+    textLyric = tkinter.Text(frame, bg="#FFFFFF", height=50)
+    S.pack(side=RIGHT, fill=Y)
     textLyric.pack(side=LEFT, fill=Y)
     S.config(command=textLyric.yview)
     textLyric.config(yscrollcommand=S.set)
-    with open('C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/lyrics.txt','r') as file:
+    with open('C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/lyrics.txt', 'r') as file:
         lyrics_text = file.read()
     lyrics = '''
     {}
@@ -110,12 +120,15 @@ def print_lyrics():
 
     textLyric.insert(END, lyrics)
 
+
 def OpenFile():
-    f = filedialog.askopenfilename(title='打开文件', filetypes=[('Music', '*.wav'), ('All Files', '*')])
-    #print(f)
+    f = filedialog.askopenfilename(title='打开文件', filetypes=[
+                                   ('Music', '*.wav'), ('All Files', '*')])
+    # print(f)
     pygame.mixer.init()
     pygame.mixer.music.load('{}'.format(f))
     pygame.mixer.music.play()
+
 
 def menu():
     menubar = Menu(master)
@@ -125,19 +138,19 @@ def menu():
     menubar.add_cascade(label="File", menu=filemenu)
     master.config(menu=menubar)
 
-    #mainloop()
+    # mainloop()
 
 
 if __name__ == '__main__':
     os.chdir('C:/Users/Lhx/Desktop/Python/MusicPlayer/版本2.0/')
-    if run('dir /B /A *.wav',shell=True):
-        run('attrib *.wav -s -h',shell=True)
-        run('del *.wav',shell=True)
+    try:
+        run('attrib *.wav -s -h', shell=True)
+        run('del *.wav', shell=True)
+    except:
+        pass
     master = tkinter.Tk()
     master.title('Music Player')
     master.geometry("700x500+200+100")
     Input_songName()
     Init()
-    #master.mainloop()
-
-
+    master.mainloop()
